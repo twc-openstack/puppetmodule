0189a43e9a70c1bd8cd745e3f5683f5437ac8be5
Devon Hubner <devon@hubner.org>
2015-07-22 15:50:24 +0000

    changed code alignment

--- tests/dbterminus.pp
+++ tests/dbterminus.pp
@@ -1,6 +1,6 @@
  class {'puppet::dbterminus':
   puppet_confdir => '/etc/puppet/puppet.conf',
   puppet_service => Service['httpd'],
-  dbport => '8081',
-  dbserver => 'test.example.com'
+  dbport         => '8081',
+  dbserver       => 'test.example.com'
   }
