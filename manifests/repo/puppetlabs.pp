119f32636b8de4608c395dd0e8b4379e21ae799c
Patrick Robinson <patrick.robinson@envato.com>
2015-09-14 19:41:47 +1000

    Fix deprecation warnings
    As per https://forge.puppetlabs.com/puppetlabs/apt key and key_content
    are deprecated

--- manifests/repo/puppetlabs.pp
+++ manifests/repo/puppetlabs.pp
@@ -7,8 +7,10 @@ class puppet::repo::puppetlabs() {
   if($::osfamily == 'Debian') {
     Apt::Source {
       location    => 'http://apt.puppetlabs.com',
-      key         => '47B320EB4C7C375AA9DAE1A01054B7A24BD6EC30',
-      key_content => template('puppet/pgp.key'),
+      key         => {
+        id      => '47B320EB4C7C375AA9DAE1A01054B7A24BD6EC30',
+        content => template('puppet/pgp.key')
+      },
     }
     apt::source { 'puppetlabs':      repos => 'main' }
     apt::source { 'puppetlabs-deps': repos => 'dependencies' }
